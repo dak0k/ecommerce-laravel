40ffea35a1648d77a03ee2fff9da07a02fcb76f3
Adaibekov Darkhan <adaibekovdarkhan5@gmail.com>
2023-05-14 00:45:55 +0500

    first steps

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,70 @@
+<!Doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- plugins:css -->
+    <link rel="stylesheet" href="{{asset('admin/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/vendors/base/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- plugin css for this page -->
+    <link rel="stylesheet" href="{{asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="{{asset('admin/css/style.css')}}">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="{{asset('admin/images/favicon.png')}}" />
+ 
+    <!-- Scripts -->
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    @livewireStyles
+</head>
+<body>
+
+    <div class="container-scroller">
+            @include('layouts.inc.admin.nav')
+        <div class="container-fluid page-body-wrapper">
+            @include('layouts.inc.admin.side')
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    @yield('content')
+                </div>
+            </div>       
+        </div>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+   
+    <script src="{{asset('admin/vendors/base/vendor.bundle.base.js')}}"></script>
+    
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    {{-- <script src="vendors/chart.js/Chart.min.js"></script> --}}
+    <script src="{{asset('admin/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{asset('admin/js/off-canvas.js')}}"></script>
+    <script src="{{asset('admin/js/hoverable-collapse.js')}}"></script>
+    <script src="{{asset('admin/js/template.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{asset('admin/js/dashboard.js')}}"></script>
+    <script src="{{asset('admin/js/data-table.js')}}"></script>
+    <script src="{{asset('admin/js/jquery.dataTables.js')}}"></script>
+    <script src="{{asset('admin/js/dataTables.bootstrap4.js')}}"></script>
+    <!-- End custom js for this page-->
+
+    @livewireScripts
+</body>
+</html>
